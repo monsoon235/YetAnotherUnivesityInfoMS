8384c0d07fa68e6b6b786f30da35b29f0ab2e906
monsoon <yjh1021317464@gmail.com>
2020-05-23 05:35:08 +0800

    add check_permission

--- college-manager/backend/api/selection.py
+++ college-manager/backend/api/selection.py
@@ -21,6 +21,30 @@ def check_params(params: dict) -> dict:
     return params
 
 
+def check_permission(func):
+    def wrapper(request: HttpRequest, *args, **kw):
+        if not request.user.is_authenticated:
+            return response_error('login required', status=403)
+        if request.user.is_staff:
+            return func(request, *args, **kw)
+        if request.method == 'GET':
+            params = check_params(request.GET.dict())
+        elif request.method == 'POST':
+            params = json.loads(request.body.decode())
+            params = check_params(params.get('where', {}))
+        else:
+            return response_error('method denied')
+        query = Selection.objects.filter(**params).values(person_id=F('student__person_id'))
+        person_id_list = list({item['person_id'] for item in query})
+        if len(person_id_list) == 0 or \
+                (len(person_id_list) == 1 and person_id_list[0] == request.user.get_username):
+            return func(request, *args, **kw)
+        return response_error('permission denied', status=403)
+
+    return wrapper
+
+
+@check_permission
 @hold_exception
 @require_http_methods(['GET'])
 @django.views.decorators.csrf.csrf_exempt
@@ -37,6 +61,7 @@ def get(request: HttpRequest):
     return response_success(list(result))
 
 
+@check_permission
 @hold_exception
 @require_http_methods(['POST'])
 @django.views.decorators.csrf.csrf_exempt
@@ -46,6 +71,7 @@ def add(request: HttpRequest):
     return general_add(Selection, params)
 
 
+@check_permission
 @hold_exception
 @require_http_methods(['GET'])
 @django.views.decorators.csrf.csrf_exempt
@@ -54,6 +80,7 @@ def delete(request: HttpRequest):
     return general_del(Selection, params)
 
 
+@check_permission
 @hold_exception
 @require_http_methods(['POST'])
 @django.views.decorators.csrf.csrf_exempt
